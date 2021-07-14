5b11b4e2a2e92e69075ea5dbfa5aacc2ba6b3543
Fernando Sosa <fst_96@hotmail.com>
2021-07-14 11:17:29 -0500

    Haciendo la maqueta de obtencion de informacion con el endpoint

--- app.js
+++ app.js
@@ -1,6 +1,52 @@
-console.log('hola mundo');
 
 const btnUser = document.querySelector('#user');
 const btnPassword = document.querySelector('#password');
 
-console.log(btnUser.value);
+
+// Obtener informacion para un Endpoint.
+
+const url = 'https://api.chucknorris.io/jokes/random';
+
+
+
+const obtenerChiste = async() => {
+
+    try {
+
+        const resp = await fetch(url);
+        const value = await resp.json();
+        
+        return value;
+        
+    } catch (error) {
+        
+        console.error('No se pudo accesar al Endpoint')
+    }
+
+}
+
+
+
+const obtenerAlgo = async() => {
+    
+    const respuesta = await obtenerChiste();
+    console.log(respuesta,'Aqui esta respuesta');
+    return respuesta;
+    
+
+}
+
+obtenerAlgo().then(console.log);
+
+
+
+
+
+
+
+
+
+
+
+
+
