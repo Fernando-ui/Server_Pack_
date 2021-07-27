42cc53f784e294d12dc3cf3b031690d1b82bc4dc
Fernando Sosa <fst_96@hotmail.com>
2021-07-27 15:07:14 -0500

    C-validacon de login

--- js/validaciones.js
+++ js/validaciones.js
@@ -2,21 +2,78 @@ const Formulario = ( ()=>{
     'use strict'
 
     const formulario =$('#formulario');
+
         formulario.on('submit', function (e){
         
             const usuario  = $('#user');
             const password = $('#child_password');
             e.preventDefault();
 
+            const iconoRight = '<i class="fas fa-check-circle"></i>';
+            const iconoFail = '<i class="fas fa-times-circle"></i>';
     
+            
+            const removerEtiquetas = ( etiqueta) => {
+            
+                $(etiqueta).remove();
+                $(usuario).css({borderColor:''});
+                $(password).css({borderColor:''});
+                
+            }
+        
+
             if(!usuario.val() || !password.val()){
     
-                window.alert('Debe de llenar usuario y contraseña');
+                console.log( $('#inputIcon').children('i'));
+                
+        
+                
+                $('#inputIcon').append(iconoFail);
+                $('#inputPassword').append(iconoFail);
+                
+                $(usuario).css({
+
+                    borderColor:'red'
+                })
+                $('#inputPassword i').css({
+                    color:'rgb(143, 32, 32)',
+                    
+                });
+                $(password).css({
+
+                    borderColor:'red'
+                })
+                
+                $('#inputIcon i').css({
+                    
+                    color:'rgb(143, 32, 32)'
+                });
+                setInterval(() => {
+                    
+                    removerEtiquetas('i');                
+                }, 1500);
                 
             }else{
-
-                console.log('todo bien');
                 
+            
+                $(password).css({
+
+                    borderColor:'green'
+                });
+
+                $(usuario).css({
+
+                    borderColor:'green'
+                });
+                
+                
+                $('#inputIcon').append(iconoRight);
+                $('#inputPassword').append(iconoRight);
+
+                setInterval(() => {
+                    
+                    removerEtiquetas('i');
+                }, 1500);
             }
         
         
